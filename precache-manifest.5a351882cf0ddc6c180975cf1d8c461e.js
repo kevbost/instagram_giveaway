1be3d8d766ba862d3439da4f90e551bd0f8d5974
Kevin Bost <kevbost@gmail.com>
2019-04-21 23:43:01 -0400

    Updates

---
+++ precache-manifest.5a351882cf0ddc6c180975cf1d8c461e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af578261946ade388835",
+    "url": "/instagram_giveaway/static/css/main.be9f2b56.chunk.css"
+  },
+  {
+    "revision": "af578261946ade388835",
+    "url": "/instagram_giveaway/static/js/main.bb0348c3.chunk.js"
+  },
+  {
+    "revision": "1205bba7d87ec4f820ac",
+    "url": "/instagram_giveaway/static/js/runtime~main.552f48a7.js"
+  },
+  {
+    "revision": "3f7be435fe86d0c00b4e",
+    "url": "/instagram_giveaway/static/css/2.684bbf19.chunk.css"
+  },
+  {
+    "revision": "3f7be435fe86d0c00b4e",
+    "url": "/instagram_giveaway/static/js/2.063af2d8.chunk.js"
+  },
+  {
+    "revision": "4603025f4e1701a53dffab550449876f",
+    "url": "/instagram_giveaway/index.html"
+  }
+];
